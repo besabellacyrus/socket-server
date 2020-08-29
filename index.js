5ea5d00f7d24a51ddd0756ff02db95840b38f910
sai <besabellacyrus@gmail.com>
2020-08-29 23:10:32 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  origins: '*:*',  
+  transports: ['polling'] 
+});
+
+// const io = require('socket.io')(http, {
+//   origins: '*:*',
+//   transports: ['polling'],
+//   pingInterval: 10000,
+//   pingTimeout: 5000,
+//   cookie: false,
+// }); 
+
+const port = 8080;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+  console.log('weee');
+});
+
+const sessionsMap = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  io.to(socket.id).emit('myId', socket.id);
+  io.emit('ows', 1);
+
+  socket.on('userIdReceived', (userId) => {
+    sessionsMap[userId] = socket.id;
+    io.to(socket.id).emit('succesRegisteer', sessionsMap);
+  }); 
+
+  socket.on('book', (e) => {
+    console.log('has a book', e);
+    socket.broadcast.emit('riderBook', e);
+  });
+  socket.on('pickedup', (e) => {
+    const socketId = sessionsMap[e.customerId];
+    io.to(socketId).emit('isPickedUp', e);
+  });
+  socket.on('completed', (e) => {
+    const socketId = sessionsMap[e.customerId];
+    io.to(socketId).emit('isCompleted', e);
+  });
+  socket.on('confirmDelivery', (e) => {
+    const socketId = sessionsMap[e.customerId];
+    io.to(socketId).emit('acceptBook', e);
+  });
+  socket.on('sendCoord', (e) => {
+    const socketId = sessionsMap[e.customerId];
+    io.to(socketId).emit('riderCoord', e);
+  });
+});
+
+http.listen(port, () => {
+  console.log('listening on *: ' + port);
+});
